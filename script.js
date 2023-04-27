af50926834496b4b344bc59c6de220c727a82836
Abhik Roy <33581435+abhik-roy@users.noreply.github.com>
2023-04-27 15:30:30 -0400

    Update script.js
    test

--- script.js
+++ script.js
@@ -1,85 +1,27 @@
-// Select all the necessary DOM elements
-const buttons = document.querySelectorAll(".calculator button");
+const calculator = document.querySelector(".calculator");
 const result = document.querySelector(".result");
+const clear = document.querySelector(".clear");
+const equals = document.querySelector(".equals");
 
-// Initialize the calculation variables
-let currentOperand = "";
-let previousOperand = "";
-let currentOperator = null;
-let shouldResetScreen = false;
-
-// Function to reset the calculator
-function resetCalculator() {
-  currentOperand = "";
-  previousOperand = "";
-  currentOperator = null;
-  shouldResetScreen = false;
-}
-
-// Function to update the calculator screen
-function updateScreen() {
-  result.value = currentOperand;
-}
-
-// Function to handle number button clicks
-function handleNumberClick(number) {
-  if (currentOperand === "0" || shouldResetScreen) {
-    currentOperand = "";
-    shouldResetScreen = false;
-  }
-  currentOperand += number;
-  updateScreen();
-}
-
-// Function to handle operator button clicks
-function handleOperatorClick(operator) {
-  if (currentOperator !== null) {
-    performCalculation();
-  }
-  currentOperator = operator;
-  previousOperand = currentOperand;
-  currentOperand = "";
-}
-
-// Function to handle equals button click
-function handleEqualsClick() {
-  performCalculation();
-  currentOperator = null;
-}
-
-// Function to perform the actual calculation
-function performCalculation() {
-  if (currentOperator === null || shouldResetScreen) return;
-  if (currentOperator === "+") {
-    currentOperand = parseFloat(previousOperand) + parseFloat(currentOperand);
-  } else if (currentOperator === "-") {
-    currentOperand = parseFloat(previousOperand) - parseFloat(currentOperand);
-  } else if (currentOperator === "*") {
-    currentOperand = parseFloat(previousOperand) * parseFloat(currentOperand);
-  } else if (currentOperator === "/") {
-    currentOperand = parseFloat(previousOperand) / parseFloat(currentOperand);
-  }
-  shouldResetScreen = true;
-  updateScreen();
-}
-
-// Function to handle clear button click
-function handleClearClick() {
-  resetCalculator();
-  updateScreen();
-}
-
-// Loop through all the buttons and add event listeners to them
+// Attach event listeners to all number and operator buttons
+const buttons = calculator.querySelectorAll("button");
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
-    if (button.classList.contains("number")) {
-      handleNumberClick(button.innerText);
-    } else if (button.classList.contains("operator")) {
-      handleOperatorClick(button.innerText);
-    } else if (button.classList.contains("equals")) {
-      handleEqualsClick();
-    } else if (button.classList.contains("clear")) {
-      handleClearClick();
+    // If clear button is clicked, clear the result display
+    if (button.classList.contains("clear")) {
+      result.innerText = "";
+    }
+    // If equals button is clicked, calculate the result and display it
+    else if (button.classList.contains("equals")) {
+      const expression = result.innerText;
+      if (expression) {
+        const answer = eval(expression);
+        result.innerText = answer;
+      }
+    }
+    // Otherwise, add the clicked button's text to the result display
+    else {
+      result.innerText += button.innerText;
     }
   });
 });
